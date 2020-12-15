5fe844bc22f4cc18271e0facd37c34a1b9f32a00
Đặng Ngọc An <72913911+ngocanstu@users.noreply.github.com>
2020-12-16 05:50:18 +0700

    AddModelUser_TimKiem

--- controllers/default/ClientController.php
+++ controllers/default/ClientController.php
@@ -192,4 +192,14 @@ class ClientController extends Controller
 
 		$this->render('buynow', $data ,$title);
 	}
+	function search(){
+		$q = "";
+		if(isset($_GET['q'])){$q = $_GET['q'];}
+		require_once 'vendor/Model.php';
+		require_once 'models/default/productModel.php';
+		$md = new productModel;
+		$data = $md->getPrds('gia',0,8,"tensp like '%".$q."%'");
+		$title = "<span id='contentTitle' data-type='search'>Kết quả tìm kiếm cho: ".$q."</span>";
+		require_once 'views/default/Products.php';
+	}
 }
