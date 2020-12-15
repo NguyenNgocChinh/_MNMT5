6385176699cc32fd2176b3f24014a5b46a014575
Nguyễn Ngọc Chinh <chinhnguyen.210399@gmail.com>
2020-12-15 21:27:06 +0700

    Hoan Thanh Cac Chuc Nang Lien Quan Den Dat Hang

--- controllers/default/ClientController.php
+++ controllers/default/ClientController.php
@@ -69,4 +69,67 @@ class ClientController extends Controller
 		}
 		echo " ".count($_SESSION['cart']);
 	}
+	
+	function order(){
+		require_once 'vendor/Model.php';
+		require_once 'models/default/productModel.php';
+		$md = new productModel;
+		$data[] = array();
+		$num = 0;
+		if(isset($_GET['num'])){$num = $_GET['num'];$_SESSION['num'] = $num;}
+		$title = 0;
+		
+		if(isset($_SESSION['cart'])){
+			for($i = 0; $i < count($_SESSION['cart']); $i++){
+				$row = $md->getPrdById($_SESSION['cart'][$i]);
+				$row['num'] = $num[$i];
+				$data[] = $row;
+				$pr = intval(preg_replace('/\s+/', '', $row['gia']));
+				$title += $num[$i]*$pr;
+			}
+		}
+		array_shift($data);
+		$this->render('order', $data, $title);
+	}
+	function orderComplete(){
+		$ten = $sdt = $quan = $dc = $type = ""; $tt = 0;
+		$num = $sp = [];
+
+		if(isset($_POST['ten'])){$ten = $_POST['ten'];}
+		if(isset($_POST['sodt'])){$sdt = $_POST['sodt'];}
+		if(isset($_POST['quan'])){$quan = $_POST['quan'];}
+		if(isset($_POST['dc'])){$dc = $_POST['dc'];}
+		if(isset($_POST['sp'])){$sp = $_POST['sp'];}
+		if(isset($_POST['num'])){$num = $_POST['num'];}
+		if(isset($_POST['type'])){$type = $_POST['type'];}
+		/*$now = date("Y-m-d h:i:s");*/
+		$now = new DateTime(null, new DateTimeZone('ASIA/Ho_Chi_Minh'));
+		$now = $now->format('Y-m-d H:i:s');
+		require_once 'vendor/Model.php';
+		require_once 'models/default/productModel.php';
+		$md = new productModel;
+		for ($i=0; $i < count($sp); $i++) {
+			$row = $md->getPrdById($sp[$i]);
+			$tt += $num[$i]*intval(preg_replace('/\s+/', '', $row['gia']));
+		}
+		date_default_timezone_set('Asia/Ho_Chi_Minh');
+		$sql = "INSERT INTO giaodich(magd,tinhtrang,user_id,user_name,user_dst,user_addr,user_phone,tongtien,date) VALUES ('',0,'','".$ten."','".$quan."','".$dc."','".$sdt."','".$tt."','".$now."')";
+		$rs = $md->exe_query($sql);
+		if($rs){
+			$last_id = $md->getLastInsertID();
+			for ($i=0; $i < count($sp); $i++){
+				$data = array($last_id, $sp[$i], $num[$i]);
+				$md->insert('chitietgd',$data);
+			}
+			echo "OK";
+			$this->render('orderComplete');
+		}
+		if(!($type == "buynow")){
+			$_SESSION['cart'] = null;
+			if(isset($_SESSION['user'])){
+				$md->delete('giohang',"user_id = '".$_SESSION['user']['id']."'");
+			}
+		}
+		$this->render('orderComplete');
+	}
 }
