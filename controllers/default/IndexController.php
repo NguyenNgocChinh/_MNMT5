ec4833c62a3f1b82b526615438560aa093fe6f42
Nguyễn Ngọc Chinh <chinhnguyen.210399@gmail.com>
2020-12-15 11:39:08 +0700

    Hoan Thanh LAYOUT Default

--- controllers/default/IndexController.php
+++ controllers/default/IndexController.php
@@ -4,9 +4,16 @@
 */
 class IndexController extends Controller
 {
+	function __construct(){
+		$this->folder = "default";
+	}
+	
 	function index()
 	{
-		echo "HELLO";
+
+		$data = "Hello";
+		$this->render('index', $data);
 	}
+
 }
 ?>
