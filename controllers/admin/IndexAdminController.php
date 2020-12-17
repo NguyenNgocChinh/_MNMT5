4e12d9b381c95e53c0ec75fe32d0419a23345565
Chau Nguyen Quoc Anh <72914631+ChauNguyenQuocAnh@users.noreply.github.com>
2020-12-17 09:26:38 +0700

    fix bug login admin

--- controllers/admin/IndexAdminController.php
+++ controllers/admin/IndexAdminController.php
@@ -36,6 +36,7 @@ class IndexAdminController extends Controller
 		require_once 'models/users/userModel.php';
 		$md = new userModel;
 		$username = $_POST['username'];
+		$password = $_POST['password'];
 		if($username == "" || $password == ""){
 			echo "Không được để trống!";
 			return 0;
